36e8097b973f541be70d628d4a7fdc9ea0e92dc4
adriana ruiz <adriannaruiz24@gmail.com>
2024-04-18 11:58:51 -0500

    add database config with prisma

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 3. Istalar dependencias ```npm install```
 4. Levantar la base de datos ```docker compose up -d```
 5. Correr las migraciones de Prisma ```npx prisma migrate dev```
+6. Ejecutar seed ```npm run seed```
 6. Correr el proyecto ```npm run dev```
 
 ## Correr en producción
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,87 @@
+import { initialData } from "./seed";
+import {prisma} from '../lib/prisma';
+
+async function main(){
+
+  // 1. Borra registros previos
+  await prisma.productImage.deleteMany(),
+  await prisma.product.deleteMany(),
+  await prisma.category.deleteMany()
+
+
+  // 2.insertar todas las categorias
+  const {categories, products} = initialData
+
+  const categoriesData = categories.map((category) => ({
+    name: category
+  }))
+
+  await prisma.category.createMany({
+    data: categoriesData
+  })
+
+  // Para insertar un dato
+  // await prisma.category.create({
+  //   data:{
+  //     name: 'shirts',
+  //   }
+  // })
+
+
+  // 3. Crear productos
+  // tomar el id de la categoria de la bd
+  const categoriesDB = await prisma.category.findMany();
+
+  
+  // recorro las categorias para obtener {categoryName: categoryId}
+  const categoriesMap = categoriesDB.reduce( (map, category) => {
+    map[ category.name.toLowerCase() ] = category.id
+    return map
+  }, {} as Record<string, string> )
+
+
+  products.forEach( async(product) => {
+    
+    const {images, type, ...prods} = product;
+    
+    // insertar productos
+    const productDB = await prisma.product.create({
+      data: {
+        ...prods,
+        categoryId: categoriesMap[type]
+      }
+    })
+
+    // insertar imagenes
+    const imagesData = images.map( image => ({
+      url: image,
+      productId: productDB.id
+    }))
+    await prisma.productImage.createMany({
+      data: imagesData
+    })
+
+  } )
+  
+  // Para insertar un producto
+  // const {images, type, ...product1} = products[0] /** con esto saca el image y el type y con esto (...product1 => me trae los demas) */
+  // await prisma.product.create({
+  //   data: {
+  //     ...product1,
+  //     categoryId: categoriesMap['shirts']
+  //   }
+  // })
+
+
+  console.log('seed ejecutado');
+}
+
+(
+  ()=>{
+
+    if(process.env.NODE_ENV === 'production') return;
+
+    main();
+
+  }
+)();
